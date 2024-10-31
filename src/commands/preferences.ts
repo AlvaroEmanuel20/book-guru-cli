66b7e56e95b4521a33ae53e9dc883f158a3a3e33
Álvaro Emanuel <alvarodiasribeiro16@gmail.com>
2024-10-31 20:16:07 -0300

    refactor: preferences edit refactor

--- src/commands/preferences.ts
+++ src/commands/preferences.ts
@@ -1,14 +1,9 @@
-import { input, select } from '@inquirer/prompts';
+import { input } from '@inquirer/prompts';
 import database from '../lib/database';
-import { languages } from '../utils/languages';
-import { genres } from '../utils/genres';
 import * as emoji from 'node-emoji';
 import colors from 'picocolors';
 
 export default async function preferences(actionType: 'view' | 'edit') {
-  const noneOfTheseOption = '> NONE OF THESE <';
-  const currentValueOption = '> CURRENT VALUE <';
-
   if (actionType === 'view') {
     const preferences = (await database.read()).preferences;
     console.log(colors.bold(colors.bgBlue('Preferences')));
@@ -24,32 +19,12 @@ export default async function preferences(actionType: 'view' | 'edit') {
     return;
   }
 
-  let language = await select<string>({
-    message: 'Select your preferred book language:',
-    choices: [...languages, noneOfTheseOption, currentValueOption],
-    loop: false,
-  });
-
-  if (language === noneOfTheseOption) {
-    language = await input({ message: 'Write your preferred book language:' });
-  }
-
-  let genre = await select<string>({
-    message: 'Select your preferred book genre:',
-    choices: [...genres, noneOfTheseOption, currentValueOption],
-    loop: false,
-  });
-
-  if (genre === noneOfTheseOption) {
-    genre = await input({ message: 'Write your preferred book genre: ' });
-  }
+  const language = await input({ message: "What's your preferred language?" });
+  const genre = await input({ message: "What's your preferred genre?" });
 
   await database.update(({ preferences }) => {
-    preferences.language =
-      language === currentValueOption ? preferences.language : language;
-
-    preferences.genre =
-      genre === currentValueOption ? preferences.genre : genre;
+    preferences.language = language;
+    preferences.genre = genre;
   });
 
   console.log(
